9bc0f3fb7a16113bc95d1637f0eac8a2ecadaa36
VivekVish1 <Vishwakarmavivekvv7084@gmail.com>
2026-09-20 22:42:09 +0530

    new: server fix

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/myshop">
         <Navbar />
         <Routes>
           <Route path="*" element={<Main />} />
